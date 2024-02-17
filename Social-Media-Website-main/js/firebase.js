94e17919767df349487caa7e3a0efee2fe3605bd
Linh Nguyen <nguyenthuylinh040205@gmail.com>
2024-02-17 17:42:30 +0700

    push first time

---
+++ Social-Media-Website-main/js/firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC1oI25VK1ptzXsBlpzaWfihjNUeQaabrM",
+  authDomain: "socialmedia-d6317.firebaseapp.com",
+  projectId: "socialmedia-d6317",
+  storageBucket: "socialmedia-d6317.appspot.com",
+  messagingSenderId: "357146023216",
+  appId: "1:357146023216:web:307a56179298fad7a124f5",
+  measurementId: "G-BLS4D5LJ9C"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
